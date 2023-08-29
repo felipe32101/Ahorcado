d1eeb24cc54a8fd872e540db9b4f89ad8e4b3c23
Raul Yesid Rojas Ballesteros <131273447+YesidRojas06@users.noreply.github.com>
2023-08-29 14:41:23 -0500

    primer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
 <template>
   <section>
-    <h1 style="color: rgb(253, 239, 214); font-size: 200px; font-family: fantasy; margin: 0;">AHORCADO</h1>
-    <button id="jugar">Jugar</button>
+    <div v-if="temas" id="">
+      <div style="display: flex; flex-direction: column; gap: 35px; align-items: center;">
+        <h1 style="color: rgb(253, 239, 214); font-size: 100px; font-family: fantasy; margin: 0;">Selecciona un tema</h1>
+        <div style="display: flex; gap: 18px; flex-wrap: wrap; width: 380px; justify-content: center;">
+          <button class="tema">Animales</button>
+          <button class="tema">Frutas</button>
+          <button class="tema">Ciudades</button>
+          <button class="tema">Colores</button>
+          <button class="tema">Marcas</button>
+        </div>
+      </div>
+    </div>
+    <div v-else id="inicio"> 
+      <h1 style="color: rgb(253, 239, 214); font-size: 200px; font-family: fantasy; margin: 0;">AHORCADO</h1>
+      <button id="jugar" @click="iniTemas">Jugar</button>
+    </div>
   </section>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
+const temas = ref(false)
+
+function iniTemas(){
+  temas.value = true
+} 
 
 </script>
 
@@ -13,13 +34,18 @@
 
 section{
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
   width: 100%;
   min-height: 100vh;
-  gap: 50px;
   background: linear-gradient(rgb(250, 189, 74), rgb(34, 34, 150));
+}
+
+#inicio{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 25px;
 }
 
 #jugar{
@@ -32,6 +58,20 @@ section{
 }
 
 #jugar:hover{
+  background: rgb(253, 239, 214);
+  cursor: pointer;
+}
+
+.tema{
+  width: 170px; 
+  height: 40px; 
+  font-size: 20px; 
+  border: 0;
+  border-radius: 5px;
+  background-color: rgb(208, 208, 255); 
+}
+
+.tema:hover{
   background: rgb(253, 239, 214);
   cursor: pointer;
 }
