b3ad49e981710399bc58ecc1377dd8b148d5413a
AngelB10 <131167121+AngelB10@users.noreply.github.com>
2023-08-29 15:36:29 -0500

    actualizacion

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
   <section>
     <div v-if="temas" id="">
-      <div style="display: flex; flex-direction: column; gap: 35px; align-items: center;">
+      <div v-if="!temaSeleccionado" class="div" style="display: flex; display: flex;flex-direction: column;align-items: center;">
         <h1 style="color: rgb(253, 239, 214); font-size: 100px; font-family: fantasy; margin: 0;">Selecciona un tema</h1>
         <div style="display: flex; gap: 18px; flex-wrap: wrap; width: 380px; justify-content: center;">
-          <button class="tema">Animales</button>
-          <button class="tema">Frutas</button>
-          <button class="tema">Ciudades</button>
-          <button class="tema">Colores</button>
-          <button class="tema">Marcas</button>
+          <button class="tema" @click="seleccionarTema('Animales')">Animales</button>
+          <button class="tema" @click="seleccionarTema('Frutas')">Frutas</button>
+          <button class="tema" @click="seleccionarTema('Ciudades')">Ciudades</button>
+          <button class="tema" @click="seleccionarTema('Colores')">Colores</button>
+          <button class="tema" @click="seleccionarTema('Marcas')">Marcas</button>
+        </div>
+      </div>
+      <div v-else>
+        <h1 style="color: rgb(253, 239, 214); font-size: 100px; font-family: fantasy; margin: 0; ">Selecciona una dificultad</h1>
+        <div style="display: flex; gap: 18px; flex-wrap: wrap; justify-content: center;display: flex; display: flex;flex-direction: column;align-items: center">
+          <button class="tema">Facil</button>
+          <button class="tema">Medio</button>
+          <button class="tema">Difcil</button>
         </div>
       </div>
     </div>
@@ -23,9 +31,15 @@
 import { ref } from 'vue';
 
 const temas = ref(false)
+const temaSeleccionado = ref(null);
 
 function iniTemas(){
-  temas.value = true
+  temas.value = true;
+  temaSeleccionado.value = null;
+}
+
+function seleccionarTema(tema){
+  temaSeleccionado.value = tema;
 } 
 
 </script>
