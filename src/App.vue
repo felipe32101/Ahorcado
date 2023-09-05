60955ef1e6bf5754c1d32081c1a0a26f833f5465
AngelB10 <131167121+AngelB10@users.noreply.github.com>
2023-09-05 13:09:14 -0500

    actualizacion

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <section>
     <div v-if="temas" id="">
-      <div v-if="!temaSeleccionado" class="div" style="display: flex; display: flex;flex-direction: column;align-items: center; gap: 35px;">
+      <div v-if="!temaSeleccionado" class="div"
+        style="display: flex; display: flex;flex-direction: column;align-items: center; gap: 35px;">
         <h1 style="color: rgb(253, 239, 214); font-size: 100px; font-family: fantasy; margin: 0;">Selecciona un tema</h1>
         <div style="display: flex; gap: 18px; flex-wrap: wrap; width: 380px; justify-content: center;">
           <button class="tema" @click="seleccionarTema('Animales')">Animales</button>
@@ -12,18 +13,18 @@
         </div>
       </div>
       <div v-else style="display: flex; flex-direction: column; gap: 35px;">
-  <h1 style="color: rgb(253, 239, 214); font-size: 100px; font-family: fantasy; margin: 0;">Selecciona una dificultad</h1>
-  <div style="display: flex; gap: 18px; flex-wrap: wrap; justify-content: center; display: flex; flex-direction: column; align-items: center;">
-    <button class="tema" @click="seleccionarDificultad('Facil')">Facil</button>
-    <button class="tema" @click="seleccionarDificultad('Medio')">Medio</button>
-    <button class="tema" @click="seleccionarDificultad('Dificil')">Dificil</button>
-    <!-- Agregar imagen después de seleccionar la dificultad -->
-    <img v-if="dificultadSeleccionada" :src="img" alt="Imagen relacionada al tema y dificultad">
-  </div>
-</div>
-      
+        <h1 style="color: rgb(253, 239, 214); font-size: 100px; font-family: fantasy; margin: 0;" v-if="!dificultadSeleccionada">Selecciona una
+          dificultad</h1>
+        <div
+          style="display: flex; gap: 18px; flex-wrap: wrap; justify-content: center; display: flex; flex-direction: column; align-items: center;">
+          <button class="tema" @click="seleccionarDificultad('Facil')" v-if="!dificultadSeleccionada">Facil</button>
+          <button class="tema" @click="seleccionarDificultad('Medio')" v-if="!dificultadSeleccionada">Medio</button>
+          <button class="tema" @click="seleccionarDificultad('Dificil')" v-if="!dificultadSeleccionada">Dificil</button>
+          <img v-if="dificultadSeleccionada" :src="img" alt="Imagen relacionada al tema y dificultad">
+        </div>
+      </div>
     </div>
-    <div v-else id="inicio"> 
+    <div v-else id="inicio">
       <h1 style="color: rgb(253, 239, 214); font-size: 200px; font-family: fantasy; margin: 0;">AHORCADO</h1>
       <button id="jugar" @click="iniTemas">Jugar</button>
     </div>
@@ -38,14 +39,14 @@ const temas = ref(false)
 const temaSeleccionado = ref(null);
 const dificultadSeleccionada = ref(null);
 
-function iniTemas(){
+function iniTemas() {
   temas.value = true;
   temaSeleccionado.value = null;
 }
 
-function seleccionarTema(tema){
+function seleccionarTema(tema) {
   temaSeleccionado.value = tema;
-} 
+}
 function seleccionarDificultad(dificultad) {
   dificultadSeleccionada.value = dificultad;
 }
@@ -56,7 +57,8 @@ img {
   width: 200px;
   height: 200px;
 }
-section{
+
+section {
   display: flex;
   justify-content: center;
   align-items: center;
@@ -65,39 +67,37 @@ section{
   background: linear-gradient(rgb(250, 189, 74), rgb(34, 34, 150));
 }
 
-#inicio{
+#inicio {
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 25px;
 }
 
-#jugar{
-  width: 170px; 
-  height: 40px; 
-  font-size: 20px; 
+#jugar {
+  width: 170px;
+  height: 40px;
+  font-size: 20px;
   border: 0;
   border-radius: 5px;
-  background-color: rgb(208, 208, 255); 
+  background-color: rgb(208, 208, 255);
 }
 
-#jugar:hover{
+#jugar:hover {
   background: rgb(253, 239, 214);
   cursor: pointer;
 }
 
-.tema{
-  width: 170px; 
-  height: 40px; 
-  font-size: 20px; 
+.tema {
+  width: 170px;
+  height: 40px;
+  font-size: 20px;
   border: 0;
   border-radius: 5px;
-  background-color: rgb(208, 208, 255); 
+  background-color: rgb(208, 208, 255);
 }
 
-.tema:hover{
+.tema:hover {
   background: rgb(253, 239, 214);
   cursor: pointer;
-}
-
-</style>
+}</style>
