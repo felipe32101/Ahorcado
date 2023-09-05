0e5a06875554aa5df79903acdf65ed8d705e5ffe
AngelB10 <131167121+AngelB10@users.noreply.github.com>
2023-09-05 16:06:03 -0500

    actualizacion

--- src/App.vue
+++ src/App.vue
@@ -13,15 +13,23 @@
         </div>
       </div>
       <div v-else style="display: flex; flex-direction: column; gap: 35px;">
-        <h1 style="color: rgb(253, 239, 214); font-size: 100px; font-family: fantasy; margin: 0;" v-if="!dificultadSeleccionada">Selecciona una
+        <h1 style="color: rgb(253, 239, 214); font-size: 100px; font-family: fantasy; margin: 0;"
+          v-if="!dificultadSeleccionada">Selecciona una
           dificultad</h1>
-        <div
-          style="display: flex; gap: 18px; flex-wrap: wrap; justify-content: center; display: flex; flex-direction: column; align-items: center;">
+        <div style="display: flex; gap: 18px; flex-wrap: wrap; justify-content: center; display: flex; flex-direction: column; align-items: center;">
           <button class="tema" @click="seleccionarDificultad('Facil')" v-if="!dificultadSeleccionada">Facil</button>
           <button class="tema" @click="seleccionarDificultad('Medio')" v-if="!dificultadSeleccionada">Medio</button>
           <button class="tema" @click="seleccionarDificultad('Dificil')" v-if="!dificultadSeleccionada">Dificil</button>
-          <img v-if="dificultadSeleccionada" :src="img" alt="Imagen relacionada al tema y dificultad">
+          <div style="display: flex;">
+            <div v-if="dificultadSeleccionada" class="vif"> 
+              <img :src="img">
+              <div>
+                <button v-for="letra in alfabeto" :key="letra">{{ letra }}</button>
+              </div>
+            </div>
+          </div>
         </div>
+
       </div>
     </div>
     <div v-else id="inicio">
@@ -39,6 +47,8 @@ const temas = ref(false)
 const temaSeleccionado = ref(null);
 const dificultadSeleccionada = ref(null);
 
+const alfabeto = ref(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']);
+
 function iniTemas() {
   temas.value = true;
   temaSeleccionado.value = null;
@@ -47,10 +57,12 @@ function iniTemas() {
 function seleccionarTema(tema) {
   temaSeleccionado.value = tema;
 }
+
 function seleccionarDificultad(dificultad) {
   dificultadSeleccionada.value = dificultad;
 }
 </script>
+
 
 <style>
 img {
@@ -100,4 +112,13 @@ section {
 .tema:hover {
   background: rgb(253, 239, 214);
   cursor: pointer;
-}</style>
+  
+}
+.vif{
+  display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 90px;
+}
+</style>
